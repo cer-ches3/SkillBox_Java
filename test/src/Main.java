e1ebf3eeda544c9eef20edc330f257e784dbb349
sergej <cer-ches3@yandex.ru>
2023-08-23 15:03:13 +0300

    test3

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,8 +1,13 @@
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime dateTime = LocalDateTime.now();
-        System.out.println("Local date and time now: " + dateTime);
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+
+        System.out.println("Дата: " + date + "\n"
+        + "Время: " + time);
     }
 }
