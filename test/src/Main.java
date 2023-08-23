e7b746d631a9a5dbe9725055b496d5c8a8f04510
sergej <cer-ches3@yandex.ru>
2023-08-23 14:57:17 +0300

    test2

--- test/src/Main.java
+++ test/src/Main.java
@@ -3,6 +3,6 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         LocalDateTime dateTime = LocalDateTime.now();
-        System.out.println(dateTime);
+        System.out.println("Local date and time now: " + dateTime);
     }
 }
