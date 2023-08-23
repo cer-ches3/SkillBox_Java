660521c29b601e5b6f37c24973e4019eae444a07
sergey <cer-ches3@yandex.ru>
2023-08-23 22:39:11 +0300

    2.4

---
+++ M2.4 Boolean operation/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int maxWeight = 5000;
+        int minCost = 1000;
+
+        int weight = 3200;
+        int cost = 1750;
+
+        boolean isDeliveryFree = true;
+
+        //№1
+//        if (weight <= maxWeight && cost >= minCost){
+//            System.out.println("Доставка бесплатная");
+//        }
+
+        //№2
+//        if (weight > maxWeight || cost < minCost){
+//            System.out.println("Доставка платная");
+//        }
+
+        //№3
+//if (!isDeliveryFree){
+//    System.out.println("Доставка бесплатная");
+//}
+
+
+    }
+}
