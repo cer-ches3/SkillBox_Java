f91e2193e6e93920740d2377b43991a76b020756
sergey <cer-ches3@yandex.ru>
2023-08-25 20:31:21 +0300

    3.2

---
+++ M3.2 Cicle for/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*for (int i = 15; i <= 45; i++){
+
+            if (i == 16){
+                System.out.println(i + " - Вы можете начать учиться на права");
+            }
+            if (i == 18){
+                System.out.println(i + " - Вы можете получать права");
+            }
+            if (i == 35){
+                System.out.println(i + " - Вы можете баллотироваться на пост призедента");
+            }
+            else {
+                System.out.println(i);
+            }*/
+
+        /*for (;;){
+            System.out.println("Введите первое число");
+            int a = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число");
+            int b = new Scanner(System.in).nextInt();
+            int result = a + b;
+            System.out.println("Сумма чисел " + a + " и " + b + " равна: " + result);
+        }*/
+
+        /*for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }*/
+
+        /*for (int i = 5; i <= 15; i++){
+            System.out.println(i);
+        }*/
+
+        /*for (int i = 10; i >= 0; i--){
+            System.out.println(i);
+        }*/
+
+        /*for (int i = 17; i >= 9; i--){
+            System.out.println(i);
+        }*/
+
+        for (int i = 0; i <= 10; i+=2){
+            System.out.println(i);
+        }
+    }
+}
