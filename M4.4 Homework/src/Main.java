ea38aa596cee8e469484b91bd5cd50c2fe7f7ba5
sergey <cer-ches3@yandex.ru>
2023-08-30 22:51:06 +0300

    4.3-4.4

---
+++ M4.4 Homework/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    /*public static void main(String[] args) {
+        int sum = calculatorSum(200, 3000, 700);
+        System.out.println(sum);
+    }
+
+    public static int calculatorSum(int a, int b, int c) {
+        return a + b + c;
+    }*/
+
+    public static void main(String[] args) {
+        int max = maxValue(55,55);
+        System.out.println(max);
+
+    }
+    public static int maxValue(int a, int b){
+        return a>b ? a : b;
+    }
+}
