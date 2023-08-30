52cd488470d7da52630ad957e9b1228f87700b47
sergey <cer-ches3@yandex.ru>
2023-08-30 19:07:08 +0300

    4.3

---
+++ M4.3 Metods/src/CalculateTriangleSquare.java
@@ -0,0 +1,12 @@
+public class CalculateTriangleSquare {
+    public static void main(String[] args) {
+        calculateTriangleSquare(3,4,5);
+
+    }
+    public static void calculateTriangleSquare(double a, double b, double c){
+        double p = (a + b + c) / 2;
+        double square =Math.sqrt(
+                p * (p - a) * (p - b) * (p - c) );
+        System.out.println(square);
+    }
+}
---
+++ M4.3 Metods/src/Square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        System.out.println("Введите число и нажмите <Enter>: ");
+        double value = new Scanner(System.in).nextDouble();
+        square(value);
+    }
+    public static void square(double value){
+        System.out.println(value * value);
+    }
+}
