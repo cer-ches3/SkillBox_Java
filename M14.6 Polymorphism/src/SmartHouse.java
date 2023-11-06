6f3bd4b3719854c138ebbec64ac845f81b83ed2b
sergey <cer-ches3@yandex.ru>
2023-11-06 13:57:54 +0300

    14.5 - 14.6

---
+++ M14.6 Polymorphism/src/SmartHouse.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class SmartHouse {
+    ArrayList<ElectricDevice> devices;
+    public SmartHouse(){
+        devices = new ArrayList<>();
+        initLightingDevices();
+    }
+    private void initLightingDevices(){
+        devices.add(new Lamp(100));
+        devices.add(new Lamp(60));
+        devices.add(new Lamp(40));
+        devices.add(new Chandelier(100, 4));
+        devices.add(new Chandelier(400, 10));
+        devices.add(new Chandelier(250, 5));
+        devices.add(new Chandelier(120, 3));
+    }
+
+    public void switchOffAllDevices(){
+        for (ElectricDevice device : devices){
+            device.switchOff();
+        }
+    }
+
+    public double getEnergyConsumption() {
+        double sum = 0;
+        for (ElectricDevice device : devices){
+            sum += device.getEnergyConsumption();
+        }
+        return sum;
+    }
+
+    public void switchOffAllLight(){
+        for (ElectricDevice device : devices){
+            if (device instanceof LightDevice){
+                device.switchOff();
+            }
+        }
+    }
+}
