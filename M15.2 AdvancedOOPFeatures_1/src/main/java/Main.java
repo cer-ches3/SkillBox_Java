c7062c84e4eea59e9a7a5ba7bc55d1462c111565
sergey <cer-ches3@yandex.ru>
2023-11-09 20:05:20 +0300

    15.5-15.6

--- M15.2 AdvancedOOPFeatures_1/src/main/java/Main.java
+++ M15.2 AdvancedOOPFeatures_1/src/main/java/Main.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -9,9 +11,24 @@ public class Main {
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         sortBySalaryAndAlphabet(staff);
-        for (Employee employee : staff) {
-            System.out.println(employee);
-        }
+//        staff.stream().filter(employee -> employee.getSalary() >= 100000).forEach(System.out ::println);
+
+        /*Stream<Integer> numbers = Stream.of(1,2,3,4,5,6,7,8,9,10);
+        numbers.filter(number -> number % 2 == 0).forEach(System.out :: println);*/
+
+        /*Integer[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        Arrays.stream(numbers).filter(number -> number % 2 == 0).forEach(System.out :: println);*/
+
+//        Stream.iterate(1, n -> n + 1).forEach(System.out::println);
+
+//        Stream.generate(() -> "aaa").forEach(System.out::print);
+
+//        staff.stream().sorted(Comparator.comparing(Employee::getSalary)).forEach(System.out::println);
+
+        staff.stream().min(Comparator.comparing(Employee::getSalary)).ifPresent(System.out::println);
+        staff.stream().max(Comparator.comparing(Employee::getSalary)).ifPresent(System.out::println);
+
+
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
