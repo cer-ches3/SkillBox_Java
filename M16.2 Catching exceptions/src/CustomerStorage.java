6721dc5248cae403c53d764e891d6d2a60a89e4a
cer-ches3 <114877440+cer-ches3@users.noreply.github.com>
2023-11-21 12:50:25 +0300

    16.2

---
+++ M16.2 Catching exceptions/src/CustomerStorage.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class CustomerStorage
+{
+    private HashMap<String, Customer> storage;
+
+    public CustomerStorage()
+    {
+        storage = new HashMap<>();
+    }
+
+    public void addCustomer(String data)
+    {
+        String[] components = data.split("\\s+");
+        if (components.length != 4){
+            throw new IllegalArgumentException("Errors in format. Correct format:\n" +
+                    "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+        String name = components[0] + " " + components[1];
+        storage.put(name, new Customer(name, components[3], components[2]));
+    }
+
+    public void listCustomers()
+    {
+        storage.values().forEach(System.out::println);
+    }
+
+    public void removeCustomer(String name)
+    {
+        storage.remove(name);
+    }
+
+    public int getCount()
+    {
+        return storage.size();
+    }
+}
